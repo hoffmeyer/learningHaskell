26bbf3ab06a1474639a1be8c6cf5e7b8b539f500
Flemming Hoffmeyer <flemming@festina-lente.dk>
2015-10-08 21:31:19 +0200

    assignment 3 and 4

--- 99problems/1-10/1-10.hs
+++ 99problems/1-10/1-10.hs
@@ -1,10 +1,22 @@
 -- 1
+myLast :: [a] -> a
 myLast [] = error "No end for empty lists"
 myLast [x] = x
 myLast (_:xs) = myLast xs
 
 -- 2
+myButLast :: [a] -> a
 myButLast [] = error "No second last element for empty lists"
 myButLast [_] = error "No second last element for lists of length 1"
 myButLast [x,y] = x
 myButLast (_:xs) = myButLast xs
+
+-- 3
+elementAt :: [a] -> Int -> a
+elementAt xs 0 = error "Index should be larger than 0"
+elementAt xs i = xs !! (i - 1)
+
+-- 4
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
