a1b6d122af1bf4888d09d1adab7853cbb40b2780
Flemming Hoffmeyer <flemmingbh@gmail.com>
2016-01-05 21:54:12 +0100

    added main

--- ranky.hs
+++ ranky.hs
@@ -14,3 +14,17 @@ instance Eq (EventData) where
 data Event = PlayerCreated Player | MatchPlayed Match
 
 type UnrankedList = [(Int, Player)]
+
+events :: [EventData]
+events =  [EventData 1 (PlayerCreated (Player 1 "TheHoff" 1000))
+          ,EventData 2 (PlayerCreated (Player 2 "Flemming" 1000))
+          ,EventData 3 (MatchPlayed (Match [1] 2 [2] 1))]
+
+
+main = do
+  putStrLn "Ranky - Scoring the elite"
+  fetchedEvents <- return events
+  putStrLn "Fetching events..."
+  putStrLn "Fetched xx events"
+  putStrLn "Applying Events"
+  putStrLn "Events applied here comes the list:"
