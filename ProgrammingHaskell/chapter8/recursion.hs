889aacb64cde867fcd76c0a37e3e7bb5e240a33a
Flemming Hoffmeyer <flemming@festina-lente.dk>
2016-02-29 21:24:55 +0100

    McCarthy 91 in chapter 8

--- ProgrammingHaskell/chapter8/recursion.hs
+++ ProgrammingHaskell/chapter8/recursion.hs
@@ -7,3 +7,8 @@ sumUp x = x + sumUp (x - 1)
 recMult :: (Integral a) => a -> a -> a
 recMult x 1 = x
 recMult x y = x + recMult x (y - 1)
+
+mc91 :: (Num a, Ord a) => a -> a
+mc91 n
+    | n > 100 = n - 10
+    | otherwise = mc91 $ mc91 (n + 11)
