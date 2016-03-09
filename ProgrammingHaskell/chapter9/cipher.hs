f9d05ea34e388122cfff08f291d37c5736ee2ec5
Flemming Hoffmeyer <flemming@festina-lente.dk>
2016-03-09 21:09:03 +0100

    Better version over caecar cipher

--- ProgrammingHaskell/chapter9/cipher.hs
+++ ProgrammingHaskell/chapter9/cipher.hs
@@ -2,4 +2,14 @@ module Cipher where
 
 import Data.Char
 
+caecar :: Int -> String -> String
+caecar n = map f
+    where f c = case generalCategory c of
+            LowercaseLetter -> addChar 'a' n c
+            UppercaseLetter -> addChar 'A' n c
+            _               -> c
 
+unCaecar n = caecar (-n)
+
+addChar :: Char -> Int -> Char -> Char
+addChar b o c = chr $ ord b + (ord c - ord b + o) `mod` 26
