d0415f78d0ad38ee9aadf6a7b2c115d4d9f83340
Flemming Hoffmeyer <flemming@festina-lente.dk>
2016-09-07 06:33:04 +0200

    functors

---
+++ ProgrammingHaskell/chapter16/exercises.hs
@@ -0,0 +1,15 @@
+module Exercises where
+
+a = fmap (+1) $ read "[1]" :: [Int]
+
+b = (fmap . fmap) (++ "lol") (Just ["Hi,", "Hello"])
+
+c = fmap (*2) (\x -> x - 2)
+
+d = fmap ((return '1' ++) . show) (\x -> [x, 1..3])
+
+e :: IO Integer
+e = let ioi = readIO "1" :: IO Integer
+        changed = read $ fmap ("123"++) (fmap show ioi)
+     in (*3) changed 
+-- 3693
