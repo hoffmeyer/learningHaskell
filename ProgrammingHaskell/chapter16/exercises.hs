7ddf8d28e147dac518dd685554ef99cd63fd7b89
Flemming Hoffmeyer <flemming@festina-lente.dk>
2016-09-25 22:04:15 +0200

    chapter 17 stuff

--- ProgrammingHaskell/chapter16/exercises.hs
+++ ProgrammingHaskell/chapter16/exercises.hs
@@ -203,4 +203,46 @@ instance Functor f => Functor (LiftItOut f) where
 data Parappa f g a = DaWrappa (f a) (g a) deriving (Eq, Show)
 
 instance (Functor f, Functor g) => Functor (Parappa f g) where
-        fmap f (DaWrappa fa ga) = LiftItOut (fmap f fa) (fmap f ga)
+        fmap f (DaWrappa fa ga) = DaWrappa (fmap f fa) (fmap f ga)
+
+--7
+data IgnoreOne f g a b =
+        IgnoringSomething (f a) (g b)
+
+instance (Functor f, Functor g) => Functor (IgnoreOne f g a) where
+        fmap f (IgnoringSomething fa gb) = IgnoringSomething fa (fmap f gb)
+
+-- 8
+data Notorious g o a t = Notorious (g o) (g a) (g t) deriving (Eq, Show)
+
+instance Functor g => Functor (Notorious g o a) where
+        fmap f (Notorious go ga gt) = Notorious go ga (fmap f gt)
+
+-- 9
+data List a = Nil
+            | Cons a (List a)
+            deriving (Eq, Show)
+
+instance Functor List where
+        fmap f Nil = Nil
+        fmap f (Cons a  list) = Cons (f a) (fmap f list)
+
+-- 10
+data GoatLord a = NoGoat
+                | OneGoat a
+                | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+
+instance Functor GoatLord where
+        fmap f NoGoat = NoGoat
+        fmap f (OneGoat a) = OneGoat (f a)
+        fmap f (MoreGoats x y z) = MoreGoats (fmap f x) (fmap f y) (fmap f z)
+
+-- 11
+data TalkToMe a = Halt
+                | Print String a
+                | Read (String -> a)
+
+instance Functor TalkToMe where
+        fmap f Halt = Halt
+        fmap f (Print s a) = Print s (f a)
+        fmap f (Read g) = Read (f . g)
