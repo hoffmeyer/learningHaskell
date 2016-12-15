de7a0531714d0b8a1ba3af2d877ac67116073a1d
Flemming Hoffmeyer <flemming@festina-lente.dk>
2016-12-15 22:01:12 +0100

    Not sure I actually understand this

--- ProgrammingHaskell/chapter18/chapter18_7_9.hs
+++ ProgrammingHaskell/chapter18/chapter18_7_9.hs
@@ -1,4 +1,8 @@
-import Control.Monad
+import           Control.Monad
 
 meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh [] _       = pure []
+meh (x : xs) f = (:) <$> f x <*> meh xs f
 
+flipType :: (Monad m) => [m a] -> m [a]
+flipType xs = meh xs (join . pure )
